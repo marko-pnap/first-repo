56d511fe5d4adcbf5d916581390da1713d4df442
Marko Aleksić <m.aleksic@outlook.com>
2021-09-30 20:56:04 +0200

    This is my first git commit.

---
+++ add-numbers.py
@@ -0,0 +1,10 @@
+#Ask for user input
+a = input('First Number:')
+b = input('Second Number:')
+
+#Add numbers
+c = int(a) + int(b)
+
+#Print the output
+print('The sum of {0} and {1} is {2}.'.format(a,b,c))
+
